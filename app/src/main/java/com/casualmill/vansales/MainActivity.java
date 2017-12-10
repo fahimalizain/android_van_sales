bbeb6451762428d75d7416a02d4762a5064041b5
Fahim Ali Zain TP <faztp12@gmail.com>
2017-12-10 17:38:39 +0300

    Basic UI

--- app/src/main/java/com/casualmill/vansales/MainActivity.java
+++ app/src/main/java/com/casualmill/vansales/MainActivity.java
@@ -3,13 +3,19 @@ package com.casualmill.vansales;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.casualmill.vansales.fragments.InvoiceFragment;
+import com.casualmill.vansales.fragments.ItemFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private ViewPager viewPager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_sales:
+                    viewPager.setCurrentItem(0);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_items:
+                    viewPager.setCurrentItem(1);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_settings:
+                    viewPager.setCurrentItem(2);
                     return true;
             }
             return false;
@@ -36,8 +42,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        LoadUI();
+    }
+
+    protected void LoadUI() {
+        // Viewpager
+        viewPager = findViewById(R.id.main_viewPager);
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return new InvoiceFragment();
+                    case 1:
+                        return new ItemFragment();
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
+        viewPager.setAdapter(adapter);
+
+        // Bottom Nav
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
---
+++ app/src/main/java/com/casualmill/vansales/fragments/InvoiceFragment.java
@@ -0,0 +1,30 @@
+package com.casualmill.vansales.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.casualmill.vansales.R;
+
+public class InvoiceFragment extends Fragment {
+
+    public InvoiceFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_invoice, container, false);
+    }
+
+}
