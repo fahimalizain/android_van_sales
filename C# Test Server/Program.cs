77c91e7f4e5f2e586bf90d155b38875934190b58
Fahim Ali Zain TP <faztp12@gmail.com>
2017-12-16 20:20:41 +0300

    Test Server Updates

--- C# Test Server/Program.cs
+++ C# Test Server/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace test_server
 {
@@ -9,6 +12,39 @@ namespace test_server
     {
         static void Main(string[] args)
         {
+            CancellationTokenSource _cancel = new CancellationTokenSource();
+            Thread t = new Thread(() =>
+            {
+                TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Loopback, 18565));
+                listener.Start();
+                Console.WriteLine("Started Listening..");
+                while (true)
+                {
+                    if (_cancel.IsCancellationRequested)
+                        break;
+
+                    if (listener.Pending())
+                    {
+                        Socket s = listener.AcceptSocket();
+                        new Thread(() =>
+                        {                            
+                            handleConnection(s);
+                        }).Start();
+                    }
+                }
+                Console.WriteLine("Cancellation Requested");
+                listener.Stop();
+            });
+            t.Start();
+            Console.ReadKey(true);
+            _cancel.Cancel();   
         }
-    }
+
+        static void handleConnection(Socket s)
+        {
+            Console.WriteLine("Incoming connection from " + s.RemoteEndPoint.ToString());
+
+            s.Close();
+        }
+    }    
 }
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Android Van Sales Sample Application
+---
+
+An app that can sync and fetch item details from a specified network stream, do sales and print the invoice.
+
+---
+## Network Sync :
+- Sync Pending Invoices
+- Fetch new stock status and overwrite the app database
+- Fetch related item data
+- Fetch Unit wise Item Price
+
+### Byte Stream Info
+The following is expected from the Server on connection: (Simple TCP Socket Connection)
+
